b6d4104e145fd4dafea675da0b535bdda33e4619
Александр <alexandr.serch.qa@gmail.com>
2026-01-25 19:27:34 +0200

    1. Создание и наполнение тестового фреймворка 2. Подключение allure-отчета и скринов 3. Наполнение README.md

--- utlis/attach.py
+++ utlis/attach.py
@@ -1,16 +1,6 @@
 import allure
 from allure_commons.types import AttachmentType
 
-# def attach_screenshot(driver, name="Скриншот"):
-#     allure.attach(
-#         driver.get_screenshot_as_png(),
-#         name=name,
-#         attachment_type=AttachmentType.PNG
-#     )
-
-import allure
-from allure_commons.types import AttachmentType
-
 def attach_screenshot(driver, name="Скриншот"):
     try:
         png = driver.get_screenshot_as_png()
